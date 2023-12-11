5de9aff18caad84ccab93ae26b7c143847251543
inadeqtfuturs <alexj.christie@gmail.com>
2023-12-11 11:57:02 -0600

    feat(docs): add todo items

--- src/adapters/vercel/index.ts
+++ src/adapters/vercel/index.ts
@@ -6,9 +6,31 @@ import { getHandleUpload } from './handleUpload'
 import { getStaticHandler } from './staticHandler'
 import { extendWebpackConfig } from './webpack'
 
+// TODO: move VercelBlobConfig to this file as `Args`
+export interface Args {}
+
+// TODO: use this as a reference
+/* {
+ *  token,
+ *  baseUrl, // RENAME
+ *  storeId,
+ *  config {
+ *    access,
+ *    optionalUrlPrefix,
+ *    addRandomSuffix,
+ *    cacheControlMaxAge
+ *  }
+ * } */
+
+console.log('@--> here')
 export const vercelBlobAdapter = (config: VercelBlobConfig): Adapter => {
+  console.log('@--> here info here')
   // read config options and pass to `handleUpload`
-  return ({ collection }): GeneratedAdapter => {
+  // TODO: generate baseURL here and pass to functions
+  // ex. const baseUrl = `https://${storeId}.${access}.${baseUrl}`
+  // TODO: conditionally tack on `optionalUrlPrefix`
+  // TODO: utilize `prefix` in the same way other adapters are
+  return ({ collection, prefix }): GeneratedAdapter => {
     return {
       handleUpload: getHandleUpload(config),
       handleDelete: getHandleDelete(config),
