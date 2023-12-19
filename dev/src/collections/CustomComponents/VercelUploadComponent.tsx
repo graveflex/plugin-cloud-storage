675195cb4c1a5184b3d601d00101558d3ea3f323
Dave Lashinsky <da.lashinsky@gmail.com>
2023-12-19 11:12:40 -0500

    feat(client-plugin): removed old file, added custom component

---
+++ dev/src/collections/CustomComponents/VercelUploadComponent.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable no-console */
+import * as React from 'react'
+import { upload } from '@vercel/blob/client'
+import { useAllFormFields, reduceFieldsToValues } from 'payload/components/forms'
+import { useConfig } from 'payload/components/utilities'
+
+
+import type { CustomSaveButtonProps } from 'payload/types'
+
+// custom react component, client-side.
+export const VercelUploadComponent: CustomSaveButtonProps = ({
+  props,
+  label,
+  save,
+  DefaultButton,
+}) => {
+  const [fields] = useAllFormFields()
+  const formData = reduceFieldsToValues(fields, true)
+
+  const payloadConfig = useConfig()
+  const optionalUrlPrefix = payloadConfig.custom.vercelConfig.optionalUrlPrefix
+  const payloadServerUrl = payloadConfig.serverURL
+
+  const handleFormSubmit = async () => {
+    save()
+    console.log('formData', formData.file)
+    const originalFileName = await formData.file.name
+    const fileName = `${
+      optionalUrlPrefix ? `${optionalUrlPrefix}/${originalFileName}` : originalFileName
+    }`
+
+    const reader = new FileReader()
+    reader.readAsArrayBuffer(formData.file)
+    reader.onload = async event => {
+      const fileData = event.target.result
+
+      try {
+        const blobResult = await upload(fileName, fileData, {
+          access: 'public',
+          handleUploadUrl: `${payloadServerUrl}/api/media/vercel-upload`,
+          contentType: formData.file.type,
+          clientPayload: 'hello-from-upload',
+        })
+
+        console.log('blobResult', blobResult)
+      } catch (error) {
+        console.log('error uploading blob', error)
+      }
+    }
+  }
+
+  return <DefaultButton props={props} label={label} save={handleFormSubmit} />
+}
